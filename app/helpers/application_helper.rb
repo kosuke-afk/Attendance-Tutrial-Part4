095f7404db9bda901e9d368214e8efdba1f900b9
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2020-11-22 06:21:28 +0000

    ２章終了 top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ページごとにタイトルを返す。
+  def full_title(page_name="")
+    base_title = "AttendanceApp"
+    if page_name.empty? # page_nameが空の場合base_titleだけを返す
+      base_title
+    else      # page_nameに値がある場合は連結した文字列を返す。
+      page_name + "|" + base_title
+    end
+  end
 end
