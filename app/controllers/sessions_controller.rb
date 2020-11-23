eef5ec26df19c5df9eb3545942131beeb7d38f7c
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2020-11-23 15:05:58 +0000

    ６章終了　基本的なログイン機能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,19 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(password: [:session][:password]) # &&はuserオブジェクトが有効なのか確認
-      
+    if user && user.authenticate(params[:session][:password]) # &&はuserが有効か確認するため
+      log_in user
+      redirect_to user
     else
-     flash.now[:danger] = "認証に失敗しました。" 
-     render :new
+      flash.now[:danger] = '認証に失敗しました。'
+      render :new
     end
   end
+  
+  def destroy
+    log_out
+    flash[:success] = "ログアウトしました。"
+    redirect_to root_url
+  end
+  
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,25 @@
 module SessionsHelper
+  
+  def log_in(user)
+    session[:user_id] = user.id # 引数に渡されたuserでログインする。 一時的にブラウザーのcookiesにuser.idを保存している。
+  end
+  
+  # セッションと@current_userを放棄します。
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+  def current_user 
+    if session[:user_id]  # session[:user_id]に値があれば、
+      @current_user ||= User.find_by(id: session[:user_id]) # @current_userの中に値があればそのまま
+    end                                                     # なければ、session[:user_id]の値から探したユーザーオブジェクトを代入
+  end
+  
+  # ログイン中ならtrue,ログインしていなければfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+  
 end
