1ed385d3fc16ae0275dedc6e3c0f6a40832829af
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2020-11-29 07:19:19 +0000

    １０章　終了 勤怠情報表示ページを作ろう

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -9,4 +9,8 @@ module AttendancesHelper
     # どれにも当てはまらなければfalseを返す。
     false
   end
+   # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
+  def working_time(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
+  end
 end
