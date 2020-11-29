7382c5bd722b96faa5d00fbab351a8fd20be4981
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2020-11-29 03:38:03 +0000

    10.4 終了　一ヶ月ごとの表示切り替え機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,6 @@ class UsersController < ApplicationController
   end
   
   def show
-    @first_day = Date.current.beginning_of_month 
-    @last_day = @first_day.end_of_month 
   end
   
   def index
